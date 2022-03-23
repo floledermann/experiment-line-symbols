ed24088bd00de1adeb9fb0a7cb2598aa8047e508
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2022-03-23 10:27:59 +0100

    disable simple-client

--- stimsrv-config.js
+++ stimsrv-config.js
@@ -1,4 +1,4 @@
-const puppeteerClient = require("stimsrv-client-puppeteer");
+//const puppeteerClient = require("stimsrv-client-puppeteer");
 
 module.exports = {
   clients: {
@@ -6,14 +6,14 @@ module.exports = {
     // in a real-world scenario you would use *one* of the two options below
     
     // stimsrv-client-puppeteer is the recommended client for old & simple browsers
-    
+    /*
     "browser-simple": puppeteerClient({
       // By default, puppeteer will open a browser window on the stimsrv server,
       // to ensure pixel-perfect rendering of the output.
       // If you want to suppress the puppeteer window and accept less accurate rendering, uncomment the next line.
       //headless: true,
     }),
-        
+    */
   }
 }
   
